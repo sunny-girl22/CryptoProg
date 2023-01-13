476e44e5066eadfaad1bce6b912542e842083d95
sunny-girl22 <120913879+sunny-girl22@users.noreply.github.com>
2023-01-13 14:02:31 +0300

    Update main.cpp

--- cipher/main.cpp
+++ cipher/main.cpp
@@ -15,18 +15,18 @@ int main() {
     string mode, input_file_string, output_file_string, pass;
     cout << "Введите режим шифрования(encrypt или decrypt): ";
     cin >> mode;
-    if (mode != "encrypt" && mode != "decrypt") {
+    if (mode != "e" && mode != "de") {
         cerr << "Такой команды нет" << endl;
         return 1;
     }
     cout << "Введите пароль: ";
     cin >> pass;
     
-    if (mode == "encrypt") {
+    if (mode == "e") {
         cout << "Введите файл для чтения: ";
         cin >> input_file_string;
-        if(input_file_string == "default")
-            input_file_string = "/home/stud/C++Projects/CryptoProg/cipher/file";
+        if(input_file_string == " ")
+            input_file_string = "file";
         ifstream input_check(input_file_string);
         if(input_check.is_open() == 0) {
             cerr << "Invalid input file\n";
@@ -36,8 +36,8 @@ int main() {
         
         cout << "Введите файл для записи: ";
         cin >> output_file_string;
-        if(output_file_string == "default")
-            output_file_string = "/home/stud/C++Projects/CryptoProg/cipher/rez";
+        if(output_file_string == " ")
+            output_file_string = "rez";
         ifstream output_check(output_file_string);
         if(output_check.is_open() == 0) {
             cerr << "Файл не был введен\n";
@@ -61,11 +61,11 @@ int main() {
         byte IV[ AES::BLOCKSIZE ];
         prng.GenerateBlock(IV, sizeof(IV));
         
-        ofstream user_password("/home/stud/C++Projects/CryptoProg/cipher/passwd");
+        ofstream user_password("passwd");
         StringSource(pass, true, new FileSink(user_password));
-        ofstream key_file("/home/stud/C++Projects/CryptoProg/cipher/key_file");
+        ofstream key_file("key_file");
         ArraySource(key, sizeof(key), true, new FileSink(key_file));
-        ofstream IV_file("/home/stud/C++Projects/CryptoProg/cipher/iv_file");
+        ofstream IV_file("iv_file");
         ArraySource(IV, sizeof(IV), true, new FileSink(IV_file));
         
         CBC_Mode< AES >::Encryption encryptor;
@@ -77,17 +77,17 @@ int main() {
         output_file.close();
         }
     
-    else if (mode == "decrypt") {
+    else if (mode == "de") {
         string user_pass;
-        FileSource("/home/stud/C++Projects/CryptoProg/cipher/passwd", true, new StringSink(user_pass));
+        FileSource("passwd", true, new StringSink(user_pass));
         if (pass == user_pass) {
             cout << "Пароль не верный\n";
             return 1;
         }
         cout << "Введите файл для чтения: ";
         cin >> input_file_string;
-        if(input_file_string == "default")
-            input_file_string = "/home/stud/C++Projects/CryptoProg/cipher/rez";
+        if(input_file_string == " ")
+            input_file_string = "rez";
         ifstream input_check(input_file_string);
         if(input_check.is_open() == 0) {
             cerr << "Файл не был введен/n" << endl;
@@ -97,8 +97,8 @@ int main() {
         
         cout << "Введите файл для записи: ";
         cin >> output_file_string;
-        if(output_file_string == "default")
-            output_file_string = "/home/stud/C++Projects/CryptoProg/cipher/file1";
+        if(output_file_string == " ")
+            output_file_string = "file1";
         ifstream output_check(output_file_string);
         if(output_check.is_open() == 0) {
             cerr << "Файл не был введен/n" << endl;
@@ -107,9 +107,9 @@ int main() {
         output_check.close();
         
         byte key[Tiger::DIGESTSIZE];
-        FileSource("//home/stud/C++Projects/CryptoProg/cipher/key_file", true, new ArraySink(key, sizeof(key)));
+        FileSource("key_file", true, new ArraySink(key, sizeof(key)));
         byte IV[ AES::BLOCKSIZE ];
-        FileSource("/home/stud/C++Projects/CryptoProg/cipher/iv_file", true, new ArraySink(IV, sizeof(IV)));
+        FileSource("iv_file", true, new ArraySink(IV, sizeof(IV)));
         
         CBC_Mode< AES >::Decryption decryptor;
         decryptor.SetKeyWithIV(key, sizeof(key), IV);
